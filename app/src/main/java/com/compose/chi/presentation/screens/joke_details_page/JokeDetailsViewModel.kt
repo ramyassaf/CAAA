cb3790e031d1403dcb765b2e856aaed6c3c12062
Ramy Assaf <rassaf>
2023-09-28 14:37:23 +0200

    - Compose State replace by Coroutines StateFlow in JokeDetailsViewModel and collectedAsState in JokeDetailsScreen (now the viewModel is compose independent & became dependent on Coroutines Flow MutableStateFlow)

--- app/src/main/java/com/compose/chi/presentation/screens/joke_details_page/JokeDetailsViewModel.kt
+++ app/src/main/java/com/compose/chi/presentation/screens/joke_details_page/JokeDetailsViewModel.kt
@@ -1,23 +1,22 @@
 package com.compose.chi.presentation.screens.joke_details_page
 
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.compose.chi.common.Resource
-import com.compose.chi.domain.model.Joke
 import com.compose.chi.domain.use_case.GetJokeUseCase
 import com.compose.chi.presentation.screens.joke_home_page.JokeHomeState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
 
 class JokeDetailsViewModel(
     private val getJokeUseCase: GetJokeUseCase
 ): ViewModel() {
 
-    private val _state = mutableStateOf(JokeHomeState())
-    val state: State<JokeHomeState> = _state
+    private val _state = MutableStateFlow(JokeHomeState())
+    val state = _state.asStateFlow()
 
     init {
         getJoke()
@@ -27,15 +26,15 @@ class JokeDetailsViewModel(
         getJokeUseCase().onEach { result ->
             when (result) {
                 is Resource.Success -> {
-                    _state.value = JokeHomeState(joke = result.data)
+                    _state.update { JokeHomeState(joke = result.data) }
                 }
                 is Resource.Error -> {
-                    _state.value = JokeHomeState(
+                    JokeHomeState(
                         error = result.message ?: "An unexpected error occurred"
                     )
                 }
                 is Resource.Loading -> {
-                    _state.value = JokeHomeState(isLoading = true)
+                    _state.update { JokeHomeState(isLoading = true) }
                 }
             }
         }.launchIn(viewModelScope)
