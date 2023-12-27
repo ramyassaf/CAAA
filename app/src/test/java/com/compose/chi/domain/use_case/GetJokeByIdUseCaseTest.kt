1fb693eb3281a9b1fe1cc180908f861de8b76489
Ramy Assaf <rassaf>
2023-12-27 21:07:43 +0100

    - [Unit Test] GetJokeByIdUseCaseTest updated and tested successfully

--- app/src/test/java/com/compose/chi/domain/use_case/GetJokeByIdUseCaseTest.kt
+++ app/src/test/java/com/compose/chi/domain/use_case/GetJokeByIdUseCaseTest.kt
@@ -1,13 +1,12 @@
 import com.compose.chi.common.Resource
 import com.compose.chi.data.remote.dto.JokeDto
-import com.compose.chi.data.remote.dto.toJoke
 import com.compose.chi.domain.model.Joke
 import com.compose.chi.domain.repository.JokeRepository
 import com.compose.chi.domain.use_case.GetJokeByIdUseCase
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.runTest
 import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.Assert.assertEquals
@@ -39,22 +38,28 @@ class GetJokeByIdUseCaseTest {
     @Test
     fun `get joke by id success`() = runTest {
         // Arrange
-        val jokeId = 1
-        val mockJoke = JokeDto(id = jokeId, punchline = "Test joke", setup = "joke setup", type = "joke type")
-        `when`(mockRepository.getJokeById(jokeId)).thenReturn(mockJoke)
+        val jokeId = "1" // Make sure jokeId is a String
+        val mockJokeDto = JokeDto(id = jokeId.toInt(), punchline = "Test joke", setup = "joke setup", type = "joke type")
+
+        `when`(mockRepository.getJokeById(jokeId)).thenReturn(mockJokeDto.toJoke())
 
         // Act
-        val result = getJokeByIdUseCase(jokeId).first()
+        val result = getJokeByIdUseCase(jokeId).toList() // Collect the flow into a list
 
         // Assert
-        assertTrue(result is Resource.Success<Joke>)
-        assertEquals(mockJoke.toJoke(), (result as Resource.Success<Joke>).data)
+        assertEquals(2, result.size) // Expect loading and success states
+        assertTrue(result[0] is Resource.Loading<Joke>)
+        assertTrue(result[1] is Resource.Success<Joke>)
+        assertEquals(mockJokeDto.toJoke(), (result[1] as Resource.Success<Joke>).data)
     }
+
+
+
 
     @Test
     fun `get joke by id HTTP error`() = runTest {
         // Arrange
-        val jokeId = 1
+        val jokeId = "1"
         val expectedErrorMessage = "HTTP 404 Response.error()" // Adjust this based on the actual error message
         val contentType = "text/plain".toMediaType()
         val errorResponseBody = expectedErrorMessage.toResponseBody(contentType)
@@ -63,26 +68,37 @@ class GetJokeByIdUseCaseTest {
         `when`(mockRepository.getJokeById(jokeId)).thenThrow(HttpException(response))
 
         // Act
-        val result = getJokeByIdUseCase(jokeId).first()
+        val result = getJokeByIdUseCase(jokeId).toList() // Collect the flow into a list
+
+        // Print the result for debugging
+        println("Actual result: $result")
 
         // Assert
-        assertTrue(result is Resource.Error<Joke>)
-        assertEquals(expectedErrorMessage, (result as Resource.Error<Joke>).message)
+        assertEquals(2, result.size) // Expect loading and error states
+        assertTrue(result[0] is Resource.Loading<Joke>)
+        assertTrue(result[1] is Resource.Error<Joke>)
+        assertEquals(expectedErrorMessage, (result[1] as Resource.Error<Joke>).message)
     }
+
 
     @Test
     fun `get joke by id network error`() = runTest {
         // Arrange
-        val jokeId = 1
+        val jokeId = "1"
         val mockRepository: JokeRepository = mockk {
             coEvery { getJokeById(jokeId) } throws IOException()
         }
 
         // Act
-        val result = GetJokeByIdUseCase(mockRepository)(jokeId).first()
+        val result = GetJokeByIdUseCase(mockRepository)(jokeId).toList() // Collect the flow into a list
+
+        // Print the result for debugging
+        println("Actual result: $result")
 
         // Assert
-        assertTrue(result is Resource.Error<Joke>)
-        assertEquals("Couldn't reach server. Check your internet connection.", (result as Resource.Error<Joke>).message)
+        assertEquals(2, result.size) // Expect loading and error states
+        assertTrue(result[0] is Resource.Loading<Joke>)
+        assertTrue(result[1] is Resource.Error<Joke>)
+        assertEquals("Couldn't reach server. Check your internet connection.", (result[1] as Resource.Error<Joke>).message)
     }
 }
