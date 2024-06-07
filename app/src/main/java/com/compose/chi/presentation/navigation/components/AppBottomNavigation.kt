8ba17582f31ae95c1dcc0b7c50e48cedf1720bf7
Ramy ASSAF <ramy.assaf@atraltech.com>
2024-06-07 16:21:31 +0200

    BottomNavigation material replaced by the new material3 NavigationBar + ExperimentalMaterial3Api removed

--- app/src/main/java/com/compose/chi/presentation/navigation/components/AppBottomNavigation.kt
+++ app/src/main/java/com/compose/chi/presentation/navigation/components/AppBottomNavigation.kt
@@ -2,23 +2,22 @@ package com.compose.chi.presentation.navigation.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppBottomNavigation(
     items: List<BottomNavItem>,
@@ -26,47 +25,46 @@ fun AppBottomNavigation(
     onItemClick: (BottomNavItem) -> Unit
 ) {
     val backStackEntry = navController.currentBackStackEntryAsState()
-    BottomNavigation(
+    NavigationBar(
         modifier = Modifier.height(72.dp),
-        backgroundColor = MaterialTheme.colorScheme.primary,
-        elevation = 5.dp
+        containerColor = MaterialTheme.colorScheme.primary,
+        tonalElevation = 5.dp,
     ) {
         items.forEach { item ->
             val selected = item.route == backStackEntry.value?.destination?.route
-            BottomNavigationItem(
+            NavigationBarItem(
                 selected = selected,
                 onClick = { onItemClick(item) },
-                // TODO: Check if it's a bug in Material3 BottomNavigation (Since selectedContentColor, unselectedContentColor are not working)
-                selectedContentColor = MaterialTheme.colorScheme.tertiary,
-                unselectedContentColor = MaterialTheme.colorScheme.onPrimary,
+                label = { Text(text = item.name) },
+                colors = NavigationBarItemColors(
+                    unselectedIconColor = MaterialTheme.colorScheme.onPrimary,
+                    unselectedTextColor = MaterialTheme.colorScheme.onPrimary,
+                    selectedIconColor = MaterialTheme.colorScheme.primaryContainer,
+                    selectedTextColor = MaterialTheme.colorScheme.primaryContainer,
+                    selectedIndicatorColor = Color.Transparent,
+                    disabledIconColor = MaterialTheme.colorScheme.tertiary,
+                    disabledTextColor = MaterialTheme.colorScheme.tertiary,
+                ),
                 icon = {
                     Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        if(item.badgeCount > 0) {
+                        if (item.badgeCount > 0) {
                             BadgedBox(badge = {
-                                Text(
-                                    text = item.badgeCount.toString(),
-                                    color = if (selected) MaterialTheme.colorScheme.tertiary else MaterialTheme.colorScheme.onPrimary
-                                )
+                                Badge() {
+                                    Text(
+                                        text = item.badgeCount.toString(),
+                                        color = if (selected) MaterialTheme.colorScheme.tertiary else MaterialTheme.colorScheme.onPrimary
+                                    )
+                                }
                             }) {
                                 Icon(
                                     imageVector = item.icon,
                                     contentDescription = item.name,
-                                    tint = if (selected) MaterialTheme.colorScheme.tertiary else MaterialTheme.colorScheme.onPrimary
                                 )
                             }
                         } else {
                             Icon(
                                 imageVector = item.icon,
                                 contentDescription = item.name,
-                                tint = if (selected) MaterialTheme.colorScheme.tertiary else MaterialTheme.colorScheme.onPrimary
-                            )
-                        }
-                        if(selected) {
-                            Text(
-                                text = item.name,
-                                textAlign = TextAlign.Center,
-                                fontSize = 10.sp,
-                                color = MaterialTheme.colorScheme.tertiary
                             )
                         }
                     }
