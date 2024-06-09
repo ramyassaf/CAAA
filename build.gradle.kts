e0d43b1a5c206d927ee4b51364829b45a6bb0155
Ramy ASSAF <ramy.assaf@atraltech.com>
2024-06-09 21:49:42 +0200

    Migrated Gradle Groovy to Gradle Kotlin DSL .kts

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        // Gradle Plugin Version
+        classpath("com.android.tools.build:gradle:8.4.1")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildFile)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "CHI"
-include ':app'
+include ("app")
