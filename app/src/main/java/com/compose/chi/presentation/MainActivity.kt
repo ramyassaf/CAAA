af9096d37b7bda654b0158faa938369dc0dff946
Ramy ASSAF <>
2026-05-17 21:07:30 +0200

    - enableEdgeToEdge added in MainActivity onCreate

--- app/src/main/java/com/compose/chi/presentation/MainActivity.kt
+++ app/src/main/java/com/compose/chi/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.compose.chi.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -31,6 +32,7 @@ import com.compose.chi.presentation.ui.theme.CHITheme
 class MainActivity : ComponentActivity(), AnalyticsLogger by AnalyticsLoggerImpl() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         // Call AnalyticsLogger Interface function
         registerLifecycleOwner(this)
