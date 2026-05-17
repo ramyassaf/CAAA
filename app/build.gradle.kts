c7c857ed97b57f87ff2c3dfdaf9e5a8b483ac832
Ramy ASSAF <>
2026-05-17 21:05:24 +0200

    - targetSdk bumped 34 to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.example.chi"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
