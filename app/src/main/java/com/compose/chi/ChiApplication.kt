b07c1630c74eb5a0670ee9a76aaafd7139445141
Ramy ASSAF <>
2026-05-18 16:17:41 +0200

    - Manual DI infrastructure removed
    - ChiApplication simplified to Koin startup
    
    - Obsolete ViewModel factory helper deleted

--- app/src/main/java/com/compose/chi/ChiApplication.kt
+++ app/src/main/java/com/compose/chi/ChiApplication.kt
@@ -1,25 +1,15 @@
 package com.compose.chi
 
 import android.app.Application
-import com.compose.chi.di.AppModule
-import com.compose.chi.di.AppModuleImpl
 import com.compose.chi.di.appKoinModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
-// ** Manual Dependency injection
 class ChiApplication : Application() {
-
-    // companion 'static' object that can be accessed across the app (active as long as app is active)
-    companion object {
-        lateinit var appModule: AppModule
-    }
 
     override fun onCreate() {
         super.onCreate()
-
-        appModule = AppModuleImpl(this)
 
         startKoin {
             androidLogger()
